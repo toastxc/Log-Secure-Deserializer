cec832681be48d03c16ae8958bb60604e0305632
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-03-09 11:52:09 +0800

    remade everything

--- src/main.rs
+++ src/main.rs
@@ -1,233 +1,82 @@
-use std::{
-    io::{Read, Write},
-    fs::File,
-};
+use lsd::log::key::deserilize;
 
+const HELP: &str = "LSD
+usage: LSD --input <input> --schema <schema>
 
-use serde_json::{Result};
-use serde::{Deserialize, Serialize};
+INPUT
+the path for log file to be deserilized
+options: file (none for /var/log/secure)
 
+SCHEMA
+File type for export
+option: JSON, TOML, YAML (none for json)";
 
-const PSW: &str = "Failed password for";
-const KEY: &str = "no matching key";
+type File = String;
 
-
-#[derive(Debug, Serialize, Deserialize)]
-struct InvalidPassword {
-    pub date: String,
-    pub hostname: String,
-    pub time: String,
-    pub user_invalid: bool,
-    pub username: String,
-    pub ip: String,
-
-    
+#[derive(Debug, Default)]
+struct Cmd {
+    schema: Option<Schema>,
+    file: Option<File>,
+    pretty: bool,
+}
+#[derive(Debug, Default)]
+enum Schema {
+    #[default]
+    Json,
+    Yaml,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct InvalidKey {
-    pub date: String,
-    pub hostname: String,
-    pub time: String,
-    pub ip: String,
-
+impl Schema {
+    pub fn from_str(input: &str) -> Self {
+        match input {
+            "json" | "JSON" => Schema::Json,
+            "yaml" | "YAML" => Schema::Yaml,
+            _ => Schema::Json,
+        }
+    }
 }
-
-
 
 fn main() {
+    let args = idontlikeargs();
 
+    let mut cmd = Cmd::default();
 
-    
-    let log = fs_to_string("log.txt")
-        .expect("Error reading log.txt");
+    if args.contains(&String::from("--help")) {
+        println!("{HELP}");
+        return;
+    }
 
-
-
-    let log_vec = log.split('\n').collect::<Vec<&str>>();
-
-    let mut allvec: Vec<Vec<String>> = vec![];
-
-
-
-    for x in 0..log_vec.len() {
-
-        let log_vec_args_str = log_vec[x].split(' ').collect::<Vec<&str>>();
-
-        let mut log_vec_args = vec![];
-
-        for y in 0..log_vec_args_str.len() {
-            log_vec_args.push(format!("{}", log_vec_args_str[y]));
+    for x in 0..args.len() {
+        match args[x].as_str() {
+            "--input" | "-i" => cmd.file = Some(args[x + 1].clone()),
+            "--schema" | "-s" => cmd.schema = Some(Schema::from_str(&args[x + 1])),
+            "--pretty" | "-p" => cmd.pretty = true,
+            _ => {}
         };
+    }
 
-        allvec.push(log_vec_args);
+    let directory = cmd.file.unwrap_or(String::from("/var/log/secure"));
+
+    if let Err(error) = std::fs::File::open(&directory) {
+        println!("File Error\n{error}\nCould not access {directory}");
+        return;
     };
 
+    let raw = deserilize(&directory);
 
+    let converted = match (cmd.schema.unwrap_or_default(), cmd.pretty) {
+        (Schema::Json, true) => serde_json::to_string_pretty(&raw).unwrap_or_default(),
+        (Schema::Json, false) => serde_json::to_string(&raw).unwrap_or_default(),
 
-
-    // this process follows 3 steps 
-    // 1: turn readable text into vectors of key words 
-    // 2: deserialize those key words into a struct
-    // 3: save the data as jsons
-
-
-    let psw: Vec<InvalidPassword> = invalid_password(allvec.clone());   
-    let inval_password_str: String = serde_json::to_string(&psw).unwrap();
-    string_to_fs("InvalidPassword.txt", &inval_password_str);
-
-    let key: Vec<InvalidKey> = invalid_key(allvec);
-    let inval_key_str: String = serde_json::to_string(&key).unwrap();
-    string_to_fs("InvalidKey.txt", &inval_key_str);
-
-            
-    // example of interpreting threat data
-    interpret();
-
-}
-
-fn interpret() {
-
-
-    let raw = fs_to_string("InvalidPassword.txt")
-        .expect("Failed to read InvalidPassword.txt");
-
-    let psw: Vec<InvalidPassword> = serde_json::from_str(&raw)
-        .expect("Failed to deserialize InvalidPassword.txt");
-
-     println!("REPORT\nThere have been {} Failed password attempts between {} and {}",
-             psw.len(), psw[0].date, psw[psw.len()-1].date);
-
-
-    let mut root   = 0; // for line 88
-    
-    for x in 0..psw.len() {
-
-        let name = &psw[x].username;
-
-        match &name as &str {
-            "root" => root += 1,
-            // expand this with your systems users!
-            _      => {},
-        };
-
+        (Schema::Yaml, _) => serde_yaml::to_string(&raw).unwrap(),
     };
-    println!("There were {} attempts for root and {} invalid user attempts",
-             root,  psw.len() - root );
-
-}
-fn invalid_key(raw: Vec<Vec<String>>) -> Vec<InvalidKey> {
-
-    
-    let mut inval: Vec<Vec<String>> = vec![];
-
-    // greper functions similarly to grep as it checks given key words against
-    // a data set
-    for x in 0..raw.len() {
- 
-        if raw[x].len() > 17 {
-            let greper = format!("{} {} {}", raw[x][12], raw[x][13], raw[x][14]);
-
-            if greper == KEY {
-                inval.push(raw[x].clone());
-            };
-        };
-    };
-
-    let mut keyvec: Vec<InvalidKey> = vec![];
-
-    // from the entries that have been 'grepered', deserialize the data
-    for x in 0..inval.len() {
-
-        let temp = InvalidKey {
-
-            date: format!("{} {}", inval[x][0].clone(), inval[x][1].clone()),
-            hostname: inval[x][3].clone(),
-            time: inval[x][2].clone(),
-            ip: inval[x][9].clone(),
-
-        };
-
-         keyvec.push(temp);
-
-    };
-
-    return keyvec;
-
-
-
-
+    println!("{converted}");
 }
 
-fn invalid_password(raw: Vec<Vec<String>>) -> Vec<InvalidPassword> {
-     
-    let mut inval: Vec<Vec<String>> = vec![];
-
-    for x in 0..raw.len() {
-
-        if raw[x].len() > 10 {
-
- 
-            let greper = format!("{} {} {}", raw[x][5], raw[x][6], raw[x][7]);
-         
-            if greper == PSW {
-                inval.push(raw[x].clone());
-            };
-        };
-    };
-
-    let mut structured_vec: Vec<InvalidPassword> = vec![];
-
-
-    for x in 0..inval.len() {
-
-
-        let (is_user_invalid, username) = match &inval[x][8] as &str{
-            "invalid" => (true, inval[x][10].clone()),
-            _         => (false, inval[x][8].clone()),
-        };
-    
-        
-        let temp = InvalidPassword {
-            date: format!("{} {}", inval[x][0].clone(), inval[x][1].clone()),
-            time: inval[x][2].clone(),
-            user_invalid: is_user_invalid,
-            hostname: inval[x][3].clone(),
-            username: username,
-            ip: inval[x][11].clone(),
-        };
-        structured_vec.push(temp);
-    };
-
-    return structured_vec
-
+fn idontlikeargs() -> Vec<String> {
+    let mut vec = Vec::new();
+    for x in std::env::args() {
+        vec.push(x);
+    }
+    vec
 }
-
-
-
-// filesystem library 
-
-fn string_to_fs(target: &str, payload: &str)  {
-
-    let mut file = File::create(target)
-        .expect("could not create file");
-
-      file.write_all(payload.as_bytes()).unwrap();
-
-}
-
-fn fs_to_string(target: &str) -> Result<String> {
-
- 
-    let mut file = File::open(target)
-        .expect("could not find file");
-
-    let mut out = String::new();
-    file.read_to_string(&mut out)
-        .expect("could to parse file");
-
-    Ok(out)
-
-}
-
-
